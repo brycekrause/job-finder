ae4a0e6dc4a276350a079367f5c56ea962be687e
imjustjayk <brycekrause@icloud.com>
2024-11-21 17:36:44 -0600

    js function declaration

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 // https://jobdataapi.com/
-const api_url = ``;
+const api_url = `https://jobdataapi.com/jobs`;
+
+async function getJobs(){
+    const response = await fetch(api_url);
+    const data = response.json();
+    document.writeln(data);
+}
+
+getJobs();
