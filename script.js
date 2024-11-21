444709ed5671f39499cd0dfeed83c09f4e521bc1
imjustjayk <brycekrause@icloud.com>
2024-11-21 17:40:05 -0600

    hotfix

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ const api_url = `https://jobdataapi.com/jobs`;
 async function getJobs(){
     const response = await fetch(api_url);
     const data = response.json();
-    document.writeln(data);
+    document.writeln(data.results);
 }
